a5b64d885df3670fe00e02fe60089d686483fca8
Caner <kurucaner@gmail.com>
2022-01-25 18:02:51 -0500

    Added essentials

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,22 @@ import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+interface IState {
+  people: {
+    name: string;
+    age: number;
+    url: string;
+    notes?: string;
+  }[];
+}
+
 function App() {
-  
+  const [people, setPeople] = useState<IState["people"]>([]);
+
+  people.map((person) => {
+    person.name;
+  });
+
   return (
     <div className="App">
       <h1>People Invited to my Party</h1>
