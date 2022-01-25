4669cf036a7b5e5ececdd1cb1c38efa9454f843c
Caner <kurucaner@gmail.com>
2022-01-25 18:16:06 -0500

    Added data and render function

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 // Style
 import "./App.css";
 // Components
-import List from './components/List';
+import List from "./components/List";
 interface IState {
   people: {
     name: string;
@@ -14,12 +14,31 @@ interface IState {
 }
 
 function App() {
-  const [people, setPeople] = useState<IState["people"]>([]);
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+      name: "Michael Jordon",
+      age: 32,
+      url: "https://www.google.com/search?q=Michael+Jordon&rlz=1C1CHBF_enUS831US831&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjNy7KHwK7XAhXC4YMKHWXoBQMQ_AUICigB&biw=1366&bih=657#imgrc=_",
+      notes: "He is a good guy",
+    },
+    {
+      name: "Lebron James",
+      age: 35,
+      url: "https://www.google.com/search?q=Lebron+James&rlz=1C1CHBF_enUS831US831&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjNy7KHwK7XAhXC4YMKHWXoBQMQ_AUICigB&biw=1366&bih=657#imgrc=_",
+      notes: "He is a living legend",
+    },
+    {
+      name: "Kobe Bryant",
+      age: 40,
+      url: "https://www.google.com/search?q=Kobe+Bryant&rlz=1C1CHBF_enUS831US831&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjNy7KHwK7XAhXC4YMKHWXoBQMQ_AUICigB&biw=1366&bih=657#imgrc=_",
+      notes: "He was a legend",
+    },
+  ]);
 
   return (
     <div className="App">
       <h1>People Invited to my Party</h1>
-      <List people={people}/>
+      <List people={people} />
     </div>
   );
 }
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -10,7 +10,25 @@ interface IProps {
 }
 
 const List = ({ people }: IProps) => {
-  return <div>I am a list</div>;
+  const renderList = () => {
+    return people.map((person) => {
+      return (
+        <div key={person.name}>
+          <h1>{person.name}</h1>
+          <h2>{person.age}</h2>
+          <img src={person.url} alt={person.name} />
+          <p>{person.notes}</p>
+        </div>
+      );
+    });
+  };
+  return (
+    <ul>
+      {people.map((person) => {
+        return <div>{person.name}</div>;
+      })}
+    </ul>
+  );
 };
 
 export default List;
